062448cda154f05c11e986ed30b29b35d62dc2c7
SaaSHub Developer <dev@saashub.com>
2026-09-21 17:04:13 +0530

    Fix PostCSS config - use CommonJS syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-import type { Config } from "postcss-load-config"
-
-const config: Config = {
+module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-export default config
+
